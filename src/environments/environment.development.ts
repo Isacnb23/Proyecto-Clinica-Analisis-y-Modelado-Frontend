a9c8426d6ad40b381ab0229b5000c551adda4694
Isacnb23 <157438923+Isacnb23@users.noreply.github.com>
2026-03-31 17:11:41 -0600

    Conexion local

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://proyecto-clinica-analisis-y-modelado-backend-production.up.railway.app/api'
+  apiUrl: 'http://localhost:5022/api'
 };
